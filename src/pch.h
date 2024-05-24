f1eaf8642771ca858d13f23d31b3a98ddac781c1
Nikita Ushakov <nikita.ushakov0000@outlook.com>
2024-05-24 22:49:50 +0100

    Create pch.h

---
+++ src/pch.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <cstdint>
+#include <limits>
+#include <utility>
+#include <functional>
+#include <optional>
+#include <span>
+#include <mutex>
+
+#include <filesystem>
+namespace fs = std::filesystem;
+
+#include <plugify/compat_format.h>
