c2c96293b45ac3839b52ff492fe33b2675679b81
Nikita Ushakov <nikita.ushakov0000@outlook.com>
2024-05-27 12:43:12 +0100

    Add abo0 to plugify lib

--- tests/example_golang_plugin/build.cmd
+++ tests/example_golang_plugin/build.cmd
@@ -1,5 +1,5 @@
 cd plugify/
-gcc -x c++ -c plugify.cc
+gcc -x c++ -c plugify.cc -D_GLIBCXX_USE_CXX11_ABI=0
 ar rc libplugify.a plugify.o
 cd ../
 go build -buildmode=c-shared -o  awesome.dll main.go
